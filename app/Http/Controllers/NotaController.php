cb9b77740bb1928f800278bfd5e65923d462e6ac
Fernando Vazquez <a329568@uach.mx>
2022-03-16 16:17:20 -0700

    Primer version

---
+++ app/Http/Controllers/NotaController.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Nota;
+use Inertia\Inertia;
+use Illuminate\Http\Request;
+use Auth;
+
+class NotaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //$notas = Nota::get();
+
+        $notas = Nota::where('users_id', Auth::id())->get();
+
+        return Inertia::render('Notas/Index', [
+            'notas' => $notas
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return Inertia::render('Notas/Create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $request->validate([
+            'titulo' => 'required',
+            'contenido' => 'required',
+        ]);
+
+        //Nota::create($request->all());
+
+        $nota = new Nota;
+        $nota->titulo = $request->titulo;
+        $nota->contenido = $request->contenido;
+        $nota->users_id = Auth::id(); //id del usuario.
+        $nota->save();
+
+
+        //return redirect()->route('noticias.index');
+        return redirect()->route('noticias.index')->with('status','se ha creado una noiticia');;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Nota  $nota
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $nota = Nota::where('id', $id())->where('users_id', Auth::id())->first();
+
+        return Inertia::render('Notas/Show', [
+            'nota' => $nota
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Nota  $nota
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $nota = Nota::where('id', $id())->where('users_id', Auth::id())->first();
+
+        return Inertia::render('Notas/Edit', [
+            'nota' => $nota
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Nota  $nota
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+
+        $request->validate([
+            'titulo' => 'required',
+            'contenido' => 'required',
+        ]);
+        
+        $nota = Nota::where('id', $id())->where('users_id', Auth::id())->first();
+
+        $nota->update($request->all());
+        return redirect()->route('noticias.index')->with('status', 'La noticia se ha actualizado.');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Nota  $nota
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $nota =  Nota::where('id',$id)->where('users_id',Auth::id())->first();
+
+        $nota->delete();
+        return redirect()->route('noticias.index')->with('status','La noticia se ha eliminado');;
+    }
+}
---
+++ resources/js/Pages/Notas/Show.vue
@@ -0,0 +1,41 @@
+<template>
+    <app-layout title = "Dashboard">
+            <template>
+                <h2 class = "font-semibold text-xl text-gray-800 leading-tight">
+                    Ver noticias
+                </h2>
+            </template>
+
+            <div class= "py-12">
+                <div class = "max-w-7x1 mx-auto sm:px-6 lg:px-8">
+                    <div class = "bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                        <inertia-link :href="route('noticias.edit', nota.id)">
+                            Editar
+                        </inertia-link>
+                        <hr>
+
+                        <p>Titulo</p>
+                        <p>{{nota.titulo}}</p>
+
+                        <p>Contenido</p>
+                        <p>{{nota.contenido}}</p>
+
+                    </div>
+                </div>
+            </div>
+    </app-layout>
+</template>
+
+<script>
+    import { defineComponent } from 'vue'
+    import AppLayout from '@/Layouts/AppLayout.vue'
+
+    export default defineComponent({
+        components: {
+            AppLayout,
+        },
+        props: {
+            nota: Object,
+        }
+    })
+</script>
