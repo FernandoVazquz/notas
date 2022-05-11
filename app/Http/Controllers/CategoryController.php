e5c66f5431d3f816fa13571669ace7e386c46b35
FernandoVazquz <a329568@uach.mx>
2022-05-11 16:08:04 -0600

    2do parcial

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -38,7 +38,7 @@ class CategoryController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store($id)
     {
         Category::create($request->all()); 
         
